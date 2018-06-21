7f761d81534203bc20c698b51327990ec7cbffd9
LolaGL <lola.gallent@gmail.com>
2018-06-21 21:29:00 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# classic-arcade-game-clone-udacity
+//////////////////
+// FROGGER GAME //
+//////////////////
+
+The user can control a given character (player) to cross the canvas and reach the water.
+The player starts the game standing in the grass and the enemies start crossing the road.
+The user will have to control de player and make her reach the water avoiding the enemies.
+If the player and the enemies collide, she will go back to the starting point.
+
+
+frontend-nanodegree-arcade-game
+===============================
+
+Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
---
+++ js/app.js
@@ -0,0 +1,120 @@
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load rowImages
+  this.sprite = 'images/enemy-bug.png';
+  //enemy moves along x and y axis
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x += this.speed * dt;
+
+  // enemies apear again after the are off the canvas at a random speed
+  if (this.x > 500){
+      this.x = -50;
+      this.speed = (Math.random() * 100)+150;
+  }
+  // collision
+  if (player.x < this.x + 80 && player.x + 80 > this.x &&
+      player.y < this.y + 60 && 60 + player.y > this.y) {
+        setTimeout(() => {
+        // reset position
+        reset(200, 400);
+      }, 250);
+    };
+};
+
+// player's position goes to beginning
+function reset (x, y) {
+  player.x = x;
+  player.y = y;
+}
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+
+var enemy1 = new Enemy(-200, 60, 200);
+var enemy2 = new Enemy(-100, 150, 200);
+var enemy3 = new Enemy(-320, 230, 200);
+var enemy4 = new Enemy(-320, 230, 100);
+
+allEnemies.push(enemy1, enemy2, enemy3, enemy4);
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+var Player = function(x, y){
+    //player moves along x and y axis
+    this.x = x;
+    this.y = y;
+    //player picture
+    this.sprite = 'images/char-cat-girl.png';
+};
+
+
+Player.prototype.update = function (dt) {
+  //player reaches water
+    if (this.y < 0) {
+      // reset position
+      setTimeout(() => {
+        reset(200, 400)
+       }, 550);
+    };
+};
+
+
+// Place the player object in a variable called player
+var player = new Player(200,400);
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+// movements and directions when keypress
+Player.prototype.handleInput = function(move){
+
+  if(move === 'up' && this.y >= 30){
+    this.y -= 85;
+  }
+  if(move === 'down' && this.y <= 350){
+    this.y += 85;
+  }
+  if(move === 'right' && this.x <= 305){
+    this.x += 100;
+  }
+  if(move === 'left' && this.x >= 100){
+    this.x -= 100;
+  };
+};
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
